78d4551db4023e13db285ac0bb52c7d35da46475
Md Ashiqur Rahman <125439782+kaziashiqur@users.noreply.github.com>
2025-10-25 13:16:23 +0600

    Update frontends.sql

--- frontends.sql
+++ frontends.sql
@@ -2,7 +2,7 @@ DELETE FROM frontends
 WHERE data_keys NOT IN (
     'seo.data', 
     'cookie.data', 
-    'policy.element', 
+    'policy_pages.element', 
     'maintenance.data', 
     'kyc.content', 
     'register_disable.content'
