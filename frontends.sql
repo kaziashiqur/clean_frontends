9d16c198ebe71157ce032c29264d25ef02baa59f
Md Ashiqur Rahman <125439782+kaziashiqur@users.noreply.github.com>
2025-10-25 12:16:08 +0600

    Update frontends.sql

--- frontends.sql
+++ frontends.sql
@@ -7,3 +7,10 @@ WHERE data_keys NOT IN (
     'kyc.content', 
     'register_disable.content'
 );
+
+
+SET  @num := 0;
+
+UPDATE frontends SET id = @num := (@num+1);
+
+ALTER TABLE frontends AUTO_INCREMENT =1;
